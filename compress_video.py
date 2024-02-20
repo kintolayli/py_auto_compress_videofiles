614ec3ec93344c7fc684a3835930dd4dd9661fa8
Ilya Lotnik <golkiper-leo@yandex.ru>
2024-02-20 19:54:51 +0300

    add new print output

--- compress_video.py
+++ compress_video.py
@@ -1,8 +1,10 @@
 import os
+import subprocess
 import time
-from watchdog.observers import Observer
-from watchdog.events import FileSystemEventHandler
 from configparser import ConfigParser
+
+from watchdog.events import FileSystemEventHandler
+from watchdog.observers import Observer
 
 script_dir = os.path.dirname(os.path.realpath(__file__))
 input_dir_name = "to_compress_video"
@@ -23,17 +25,20 @@ else:
 
 os.startfile(input_dir_path)
 folder_to_watch = config.get("Settings", "folder_to_watch")
-allowed_video_formats_array = ("webm",)
+result_string_array = []
 
 
 def allowed_video_formats(path):
     path_endswith = path.split('.')[-1]
-    return path_endswith in allowed_video_formats_array
+    return path_endswith in ("webm",)
 
 
 def convert_video(input_files_path, output_files_path):
-    command = f'ffmpeg -i "{input_files_path}" -c:v libx264 -crf 24 -c:a libmp3lame -q:a 2 -vsync 2 "{output_files_path}"'
-    os.system(command)
+    command = ["ffmpeg", "-i", input_files_path, "-c:v", "libx264", "-crf",
+               "24", "-c:a", "libmp3lame", "-q:a", "2", "-vsync", "2",
+               output_files_path]
+    process = subprocess.Popen(command)
+    process.wait()
 
 
 class MyHandler(FileSystemEventHandler):
@@ -66,11 +71,15 @@ class MyHandler(FileSystemEventHandler):
                     current_size = os.path.getsize(event.src_path)
                     if current_size == self.last_size:
                         convert_video(event.src_path, new_filepath)
+
                         original_file_size = round(current_size / 1024, 2)
-                        compressed_file_size = round(os.path.getsize(new_filepath) / 1024, 2)
-                        compression_value = round(original_file_size / compressed_file_size, 2)
-                        print(f"\n\nОбработка завершена. Итоговый файл можно "
-                              f"найти по пути {new_filepath}\n\nНазвание оригинального файла: {input_filename}\nРазмер оригинального файла: {original_file_size} МБ\n\nНазвание нового файла: {new_filename}\nРазмер нового файла: {compressed_file_size} МБ\nВеличина сжатия (в ед.): {compression_value}")
+                        compressed_file_size = round(
+                            os.path.getsize(new_filepath) / 1024, 2)
+                        compression_value = round(
+                            original_file_size / compressed_file_size, 2)
+                        result = (f"Название оригинального файла: {input_filename}\nРазмер оригинального файла: {original_file_size} МБ\nНазвание нового файла: {new_filename}\nРазмер нового файла: {compressed_file_size} МБ\nВеличина сжатия (в ед.): {compression_value}")
+                        result_string_array.append(result)
+
                         os.unlink(event.src_path)
                         break
                     else:
@@ -79,6 +88,14 @@ class MyHandler(FileSystemEventHandler):
                         time.sleep(2)
                 except Exception as e:
                     print(f"Ошибка при проверке размера файла: {e}")
+
+            if len(result_string_array) != 0:
+                print(f"\n\nОбработка завершена.\nОбработано файлов: {len(result_string_array)}\nФайл(ы) можно найти по пути {new_filepath_folder}\n")
+                print(50 * "-")
+                for number, line in enumerate(result_string_array, 1):
+                    print(f"Файл {number}:\n")
+                    print(line)
+                    print(50 * "-")
 
 
 if __name__ == "__main__":
